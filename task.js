8ece18e733b64c487153f4f8aece94578bce8662
pmw0310 <pmw0310@naver.com>
2022-05-29 22:48:25 +0900

    input number

--- task.js
+++ task.js
@@ -1,7 +1,20 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const multi = (num = 2) => {
     for (i = 1; i <= 9; i++) {
         console.log(`${num} x ${i} = ${num * i}`);
     }
 };
 
-multi();
+console.log('Please enter a number.');
+
+rl.on('line', (line) => {
+    const num = Number(line);
+    multi(num);
+    rl.close();
+});
